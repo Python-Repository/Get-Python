410249d2da748170ddd3fc19fcba7afc558ce230
ARMENIO MAURICIO FERREIRA NETO <AMNETO@bbmapfre.com.br>
2018-12-19 17:42:03 -0200

    Get Python

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Learn-Python
+
+This project was done to refine my studies on responsiveness.
+
+![alt text](https://i.imgur.com/3KmI0gE.png)
+
+## Getting Started
+
+```
+-git clone https://github.com/armenio2/Get-Phyton.git
+-pip install Flask
+-python foo.py
+-http://127.0.0.1:5000/getdata
+```
+
+## Built With
+
+* [PYTHON]
+* [FLASK]
+
+## Contributing
+
+http://flask.pocoo.org/docs/1.0/
+
+https://www.python.org/
+
+https://www.w3schools.com/python/
--- foo.py
+++ foo.py
@@ -1,1 +1,37 @@
-#learning python... 123
+from flask import Flask, request, make_response
+from random import randint
+import random
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/getdata", methods=["GET"])
+def produto():
+    final_json = {}
+    final_json["nome"] = ["armenio", "foo"]
+    final_json["idUser"] = randomNumber()
+    final_json["situacao"] = "iniciada"
+    final_json["vigencia"] = "{}/{}/{} ate {}/{}/{}".format(randint(0, 26), randint(
+        1, 12), randint(1980, 2018), randint(0, 26), randint(1, 12), randint(2019, 2030))
+    final_json["keyInteger"] = randomNumberInteger()
+    response = make_response(json.dumps(final_json))
+    response.headers["Content-Type"] = "application/json"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+
+def randomNumber():
+    return random.random()*random.randint(0, 99)
+
+
+def randomNumberInteger():
+    return random.randint(100000000000, 999999999999)
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
